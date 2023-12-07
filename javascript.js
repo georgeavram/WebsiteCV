6e1574c7b454d013e80ad79be86397f4d69f1948
George Avram <X00173481@myTUDublin.ie>
2023-12-07 10:00:56 +0000

    Starting up Repo for CV, Current half way through website development

---
+++ javascript.js
@@ -0,0 +1,32 @@
+
+// sets year 4 grades to default to display
+document.getElementById('section1').classList.add('active');
+       
+// toggles to show years for grades
+function toggleContent(sectionId) {
+  // Hide content
+  var sections = document.querySelectorAll('.content-section');
+  sections.forEach(function(section) {
+    section.classList.remove('active');
+  });
+
+  // Show content
+  var selectedSection = document.getElementById(sectionId);
+  selectedSection.classList.add('active');
+}
+
+
+// used to switch color for grades button
+document.addEventListener('DOMContentLoaded', function () {
+  var gradesButton = document.getElementById('gradesButton');
+
+  gradesButton.addEventListener('click', function () {
+      gradesButton.classList.toggle('collapsed');
+  });
+  gradesButton.addEventListener('show.bs.collapse', function () {});
+  gradesButton.addEventListener('hide.bs.collapse', function () {});
+});
+
+
+
+
